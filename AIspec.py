dc585bf5867630a00f785c9c482e4b9c4a22b8f7
Jordds <83397869+Jordds@users.noreply.github.com>
2022-11-22 10:56:42 +0100

    r

--- AIspec.py
+++ AIspec.py
@@ -8,3 +8,4 @@ def AI():
     st.markdown("""
     my 4th semester at Fontys was my specialisation. I chose for AI out of interest in the subject. You hear a lot about it, and I wanted to learn a more about it. The subject in the semester was machine learning and applying ML algorithms to data. We got taught us a methodology when going through ML projects. It is about more then just building applying an algorithm. 
     """)
+    st.image()
